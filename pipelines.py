b9be00881a73c4022f598b10bc5eed16b8c2b5d5
Divyanshu <k.divyanshu@iitg.ernet.in>
2017-04-23 12:43:44 +0530

    Add more pipelines

--- pipelines.py
+++ pipelines.py
@@ -1,5 +1,5 @@
-import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import chi2, mutual_info_classif, SelectKBest
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
@@ -8,13 +8,39 @@ from sklearn.svm import SVC
 from preprocess import TextPreProcessTransformer
 
 
-pipeline = Pipeline([
+pipeline_lr = Pipeline([
     ('preprocess', TextPreProcessTransformer(stem=True)),
-    # TfidfVectorizer is equivalent to CountVectorizer followed 
-    # by TfidfTransformer.
     ('tfidf_vec', TfidfVectorizer(ngram_range=(1, 3), use_idf=False)),
-    #('classifier', MultinomialNB())
-    #('classifier', SVC(kernel='linear', probability=True))
-    ('clf', SGDClassifier())
+    ('fs', SelectKBest(chi2, k=20)),
+    ('classifier', LogisticRegression())
+    # ('clf_svc', SVC(kernel='linear'))
+    # ('clf', SGDClassifier(penalty="elasticnet"))
 ])
 
+pipeline_mnb = Pipeline([
+    ('preprocess', TextPreProcessTransformer(stem=True)),
+    ('tfidf_vec', TfidfVectorizer(ngram_range=(1, 3), use_idf=False)),
+    ('fs', SelectKBest(chi2, k=20)),
+    ('classifier', MultinomialNB())
+    # ('clf_svc', SVC(kernel='linear'))
+    # ('clf', SGDClassifier(penalty="elasticnet"))
+])
+
+pipeline_svc = Pipeline([
+    ('preprocess', TextPreProcessTransformer(stem=True)),
+    ('tfidf_vec', TfidfVectorizer(ngram_range=(1, 3), use_idf=False)),
+    # ('fs', SelectKBest(chi2, k=20)),
+    ('fs', SelectKBest(mutual_info_classif, k=1000)),
+    # ('classifier', MultinomialNB())
+    ('clf_svc', SVC(kernel='linear'))
+    # ('clf', SGDClassifier(penalty="elasticnet"))
+])
+
+pipeline_sgdc = Pipeline([
+    ('preprocess', TextPreProcessTransformer(stem=True)),
+    ('tfidf_vec', TfidfVectorizer(ngram_range=(1, 3), use_idf=False)),
+    ('fs', SelectKBest(chi2, k=20)),
+    # ('classifier', MultinomialNB())
+    # ('clf_svc', SVC(kernel='linear'))
+    ('clf', SGDClassifier())
+])
