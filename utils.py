71363403e1cd620fecffb8aa83412e9632363198
Divyanshu <k.divyanshu@iitg.ernet.in>
2017-04-23 12:44:47 +0530

    Add check for Date column

--- utils.py
+++ utils.py
@@ -1,11 +1,28 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import roc_curve, auc
 
 from constants import *
 
 
 def read_data(drop_date=True, csv_fpath=TRAINING_CSV):
-    df= pd.read_csv(csv_fpath)
-    if 'Date' in df.columns.values:
+    df = pd.read_csv(csv_fpath)
+    if drop_date and 'Date' in df.columns.values:
         df = df.drop(labels=['Date'], axis=1)
     return df
 
+
+def save_roc_curve(actual, predictions, fname="roc.png"):
+    fp_rate, tp_rate, thresholds = roc_curve(actual, predictions)
+    roc_auc = auc(fp_rate, tp_rate)
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fp_rate, tp_rate, 'b', label='AUC = %0.2f' % roc_auc)
+    plt.legend(loc='lower right')
+    plt.plot([0, 1], [0, 1], 'r--')
+    plt.xlim([-0.1, 1.2])
+    plt.ylim([-0.1, 1.2])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.savefig(fname, bbox_inches='tight')
+    # plt.show()
+    return roc_auc
