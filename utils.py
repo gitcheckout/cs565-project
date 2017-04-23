4bd19a187f4e422e1ad5737717622ab1c16bd016
Divyanshu <k.divyanshu@iitg.ernet.in>
2017-04-23 19:10:53 +0530

    Add load_bw utility method

--- utils.py
+++ utils.py
@@ -12,6 +12,18 @@ def read_data(drop_date=True, csv_fpath=TRAINING_CSV):
     return df
 
 
+def load_bw(fpath):
+    fconn = open(fpath, 'r')
+    bw_list = []
+    for i, line in enumerate(fconn):
+        if i != 0:
+            insult = (line.strip().split(":")[0]).strip('"')
+            bw_list.append(insult)
+    #print(len(bw_list))
+    #print(bw_list)
+    return bw_list
+
+
 def save_roc_curve(actual, predictions, fname="roc.png"):
     fp_rate, tp_rate, thresholds = roc_curve(actual, predictions)
     roc_auc = auc(fp_rate, tp_rate)
